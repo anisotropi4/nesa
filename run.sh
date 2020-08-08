691fcbe4776ce1d232e9b0124c2a238fa44d9b1e
Will Deakin <will.deakin@crinstitute.org.uk>
2020-08-08 21:29:09 +0100

    update following route-clearance re-write

--- run.sh
+++ run.sh
@@ -1,36 +1,66 @@
-#!/bin/bash -x
+#!/bin/bash
 
 IFS=$'\012'
 
-for FILE in $(ls download/*Sectional\ Appendix*.pdf | sed 's/^download\///')
-do
-    echo "${FILE}	$(echo ${FILE} | sed 's/ Sectional Appendix .*.pdf//; s/[,)(]//g; s/ /-/g')"
-done | jq -Rsc 'split("\n")[0:-1] | map([split("\t")][] | {(.[1]): (.[0])})' > section-list.json
+source venv/bin/activate
+export NESADIR=${PWD}
+export PATH=${PATH}:${NESADIR}
 
-for i in $(jq -c '.[]' section-list.json)
-do
-    ROUTE=$(echo ${i} | jq -r 'keys[]')
-    for j in download pdf tsv images work output
+if [ ! -s section-list.json ]; then
+    for FILEPATH in $(ls download/*Sectional\ Appendix*.pdf)
     do
-        if [ ! -d ${ROUTE}/${j} ]; then
-            mkdir -p ${ROUTE}/${j}
+        FILENAME=$(basename ${FILEPATH})
+        ROUTE=$(echo ${FILENAME} | sed 's/ Sectional Appendix .*.pdf//; s/[,)(]//g; s/ /-/g')
+        echo "{"\"${ROUTE}\": \"${FILENAME}\""}"
+    done | jq -cs '.' > section-list.json
+fi
+
+for ROUTE in $(jq -r '.[] | keys[]' section-list.json)
+do
+    echo Processing ${ROUTE}
+    for i in pdf images tsv txt work stage output report
+    do
+        if [ ! -d ${ROUTE}/${i} ]; then
+            mkdir -p ${ROUTE}/${i}
         fi
     done
-
-    FILE=$(echo ${i} | jq -r 'values[]')
-    if [ ! -f "${ROUTE}/pdf/${FILE}" ]; then
-        ln "download/${FILE}" "${ROUTE}/download/${FILE}"
+    FILENAME=$(jq -r '.[] | select(. | keys[] == "'${ROUTE}'") | .[]' section-list.json)
+    if [ -z "$(ls -A ${ROUTE}/pdf/)" ]; then
+        echo Extract PDF pages
+        (cd ${ROUTE}/pdf; pdfseparate "../../download/${FILENAME}" pg_%04d.pdf)
     fi
-
-    if [ ! -f ${ROUTE}/pdf/pg_0001.pdf ]; then
-         (cd ${ROUTE}/pdf; pdfseparate "../download/${FILE}" pg_%04d.pdf)
-    fi
-done
-
-for SCRIPT in ocr3.sh tabula.sh
-do
-    for ROUTE in $(jq -cr '.[] | keys[]' section-list.json)
+    for i in ${ROUTE}/pdf/*.pdf
     do
-        ./${SCRIPT} ${ROUTE}
+        echo ${i}
+        FILESTUB=$(basename ${i} | sed 's/.pdf//')
+        PDFPATH=${ROUTE}/work/${FILESTUB}.pdf
+        if [ ! -f ${PDFPATH} ]; then
+            N=$(pdfimages -list ${i} | wc -l)
+            if [ ${N} = 2 ]; then
+                gs -q -dSAFER -dEmbedAllFonts=true -dNOPAUSE -dBATCH -dPDFA=1 -dNOOUTERSAVE -sProcessColorModel=DeviceCMYK -sPDFACompatibilityPolicy=1 -sDEVICE=pdfwrite -sOutputFile=${PDFPATH} ${i}
+                qpdf --replace-input --stream-data=uncompress ${PDFPATH}
+                if [ ! -f output/${FILESTUB}.pdf ]; then
+                    (cd ${ROUTE}; filter_pdf.py work/${FILESTUB}.pdf)
+                fi
+            fi
+        fi
+        TXTPATH=${ROUTE}/txt/${FILESTUB}.txt
+        if [ ! -f ${TXTPATH} ]; then
+            pdftotext -layout -nopgbrk ${i} ${TXTPATH}
+        fi
     done
+    if [ -z "$(ls -A ${ROUTE}/report/)" ]; then
+        echo Extract text
+        ./collate-gauge.py ${ROUTE}
+    fi
+ done
+
+for ROUTE in $(jq -r '.[] | keys[]' section-list.json)
+do
+    echo ${ROUTE}
+    FILENAME=$(jq -r '.[] | select(. | keys[] == "'${ROUTE}'") | .[]' section-list.json)
+    if [ -z "$(ls -A ${ROUTE}/images/)" ]; then
+        echo Create PNG images
+        pdftoppm -r 300 -png "download/${FILENAME}" ${ROUTE}/images/pg
+    fi
 done
