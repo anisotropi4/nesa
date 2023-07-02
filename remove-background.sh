036567c3a1d0b6e1407b0dac8b1f6f574c640819
Will Deakin <will.deakin@crinstitute.org.uk>
2023-07-02 10:25:36 +0100

    initial commit

---
+++ remove-background.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+IFS=$'\012'
+ROUTE=$1
+export NESADIR=${PWD}
+export PATH=${PATH}:${NESADIR}
+
+source venv/bin/activate
+
+for i in ${ROUTE}/pdf/*.pdf
+do
+    FILESTUB=$(basename ${i} | sed 's/.pdf//')
+    PDFPATH=${ROUTE}/work/${FILESTUB}.pdf
+    if [ ! -f ${PDFPATH} ]; then
+        N=$(pdfimages -list ${i} | wc -l)
+        if [ ${N} = 2 ]; then
+            echo -n ${i}
+            echo -n " remove grey background"
+            gs -q -sstdout=/dev/null -dSAFER -dEmbedAllFonts=true \
+               -dNOPAUSE -dBATCH -dPDFA=1 -dNOOUTERSAVE \
+               -sProcessColorModel=DeviceGray -sColorConversionStrategy=Gray \
+               -sPDFACompatibilityPolicy=1 -sDEVICE=pdfwrite \
+               -sOutputFile=${PDFPATH} ${i} 2> /dev/null
+            qpdf --replace-input --stream-data=uncompress ${PDFPATH}
+            (cd ${ROUTE};
+             filter_pdf.py work/${FILESTUB}.pdf;
+             qpdf --no-warn stage/${FILESTUB}.pdf output/${FILESTUB}.pdf)
+            echo
+        fi
+    fi
+done
