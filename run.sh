6c76c5da299975799b5acceb6939259dac251e3d
Will Deakin <will.deakin@crinstitute.org.uk>
2023-07-02 16:16:38 +0100

    update with fixed format pdftotxt txt files

--- run.sh
+++ run.sh
@@ -2,15 +2,24 @@
 
 IFS=$'\012'
 
-source venv/bin/activate
 export NESADIR=${PWD}
 export PATH=${PATH}:${NESADIR}
 
+if [ ! -d venv ]; then
+    echo Set up python3 virtual environment
+    python3 -m venv venv
+    source venv/bin/activate
+    pip3 install --upgrade pip
+    pip3 install -r requirements.txt
+else
+    source venv/bin/activate
+fi
+
 if [ ! -s section-list.json ]; then
-    for FILEPATH in $(ls download/*Sectional\ Appendix*.pdf)
+    for FILEPATH in $(ls download/*.pdf)
     do
         FILENAME=$(basename ${FILEPATH})
-        ROUTE=$(echo ${FILENAME} | sed 's/ Sectional Appendix .*.pdf//; s/[,)(]//g; s/Sussex and Wessex/Sussex-Wessex/; s/ /-/g')
+        ROUTE=$(echo ${FILENAME} | sed 's/ Sectional Appendix .*.pdf//; s/[,)(]//g; s/Sussex and Wessex/Sussex-Wessex/; s/Western North.*/Western North/; s/ /-/g')
         echo "{"\"${ROUTE}\": \"${FILENAME}\""}"
     done | jq -cs '.' > section-list.json
 fi
@@ -18,7 +27,7 @@ fi
 for ROUTE in $(jq -r '.[] | keys[]' section-list.json)
 do
     echo Processing ${ROUTE}
-    for i in pdf images tsv txt ftxt raw work stage output report dump
+    for i in pdf images tsv txt raw work stage output report 
     do
         if [ ! -d ${ROUTE}/${i} ]; then
             mkdir -p ${ROUTE}/${i}
@@ -27,28 +36,24 @@ do
     FILENAME=$(jq -r '.[] | select(. | keys[] == "'${ROUTE}'") | .[]' section-list.json)
     if [ -z "$(ls -A ${ROUTE}/pdf/)" ]; then
         echo Extract PDF pages ${ROUTE}
-        (cd ${ROUTE}/pdf; pdfseparate "../../download/${FILENAME}" pg_%04d.pdf)
+        (cd ${ROUTE}/pdf; pdfseparate "../../download/${FILENAME}" pg_%04d.pdf 2> /dev/null)
     fi    
     echo Process ${ROUTE} PDF pages
     ./remove-background.sh ${ROUTE}
     for i in ${ROUTE}/pdf/*.pdf
     do
         FILESTUB=$(basename ${i} | sed 's/.pdf//')
-        TXTPATH=${ROUTE}/txt/${FILESTUB}.txt
-        if [ ! -f ${TXTPATH} ]; then            
-            pdftotext -layout -nopgbrk ${i} ${TXTPATH}
-        fi
         PDFPATH=${i}
         OUTPUTPATH=${ROUTE}/output/${FILESTUB}.pdf
         if [ -f ${OUTPUTPATH} ]; then
             PDFPATH=${OUTPUTPATH}
         fi
-        TXTPATH=${ROUTE}/ftxt/${FILESTUB}.txt
+        TXTPATH=${ROUTE}/txt/${FILESTUB}.txt
         if [ ! -f ${TXTPATH} ]; then
-           pdftotext -fixed 2.8 -nopgbrk ${PDFPATH} ${TXTPATH}
+            pdftotext -fixed 8 -nopgbrk ${PDFPATH} ${TXTPATH}
         fi
     done
-    if [ -z "$(ls -A ${ROUTE}/report/)" ]; then
+    if [ -z "$(ls -A ${ROUTE}/*-clearance.xlsx 2> /dev/null)" ]; then
         echo Create gauge report ${ROUTE}
         ./collate-gauge2.py ${ROUTE}
     fi
@@ -62,10 +67,10 @@ do
     do
         FILESTUB=$(basename ${i} | sed 's/.pdf//')
         if [ ! -f ${ROUTE}/images/${FILESTUB}.png ]; then
-                echo -n ${ROUTE} ${FILESTUB}.png
-            echo -n " generate"
+            #echo -n ${ROUTE} ${FILESTUB}.png
+            #echo -n " generate"
             pdftoppm -singlefile -r 300 -png ${i} ${ROUTE}/images/${FILESTUB}
-            echo
+            #echo
         fi
     done
 done
